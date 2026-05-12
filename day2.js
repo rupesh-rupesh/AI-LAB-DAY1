6344446d48c2b9a0b870182b334808ea503420a5
rupesh-rupesh <rupeshks49061@gmail.com>
2026-05-12 12:03:29 +0530

    day 2 commit

---
+++ day2.js
@@ -0,0 +1,20 @@
+ 
+    function showData(event) {
+        event.preventDefault();
+           let name = document.getElementById("name").value;
+
+    let email = document.getElementById("email").value;
+    
+    alert(
+        "Registration Successful!\n\n" +
+        "Name: " + name + "\n" +
+        "Email: " + email
+    ); 
+    }
+
+    
+
+
+
+    
+ 
